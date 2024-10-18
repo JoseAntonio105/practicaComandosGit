435a181269de60e15bd7c31ad98adf3e9b260b6b
José Antonio Marín <cambalachemf@gmail.com>
2024-10-18 08:48:06 +0200

    Multiplicación

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -2,5 +2,6 @@ public class Calculadora {
 	public static void main(String[] args) {
 		System.out.println("1. Suma");
 		System.out.println("2. Resta");
+		System.out.println("3. Multiplicación");
 	}
 }
